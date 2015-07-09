7440022e047f08163d70247b793ab424c30b0c08
tcnksm <nsd22843@gmail.com>
2015-07-10 01:26:35 +0900

    Limit OS/Arch to build

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,6 +4,11 @@ set -e
 DIR=$(cd $(dirname ${0})/.. && pwd)
 cd ${DIR}
 
+XC_ARCH=${XC_ARCH:-386 amd64}
+XC_OS=${XC_OS:-darwin linux windows}
+
 rm -rf pkg/
 gox \
+    -os="${XC_OS}" \
+    -arch="${XC_ARCH}" \
     -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"
